5fb11668b284d2170176ebb018df7bfeef2a1c94
Farouk Hamadi <faroukhamadi1@outlook.com>
2022-06-01 02:39:10 +0100

    add grid

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,15 @@
 <script lang="ts">
-	export let name: string;
+  import Board from './Board.svelte';
+  // export let name: string;
+  let name = 'Local State';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <Board />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main {
+    height: 100%;
+  }
 </style>
---
+++ src/Board.svelte
@@ -0,0 +1,126 @@
+<script lang="ts">
+</script>
+
+<div class="wrapper">
+  <div class="board">
+    <div class="cell 1" />
+    <div class="cell 2" />
+    <div class="cell 3" />
+    <div class="cell 4" />
+    <div class="cell 5" />
+    <div class="cell 6" />
+    <div class="cell 7" />
+    <div class="cell 8" />
+    <div class="cell 9" />
+    <div class="cell 10" />
+    <div class="cell 11" />
+    <div class="cell 12" />
+    <div class="cell 13" />
+    <div class="cell 14" />
+    <div class="cell 15" />
+    <div class="cell 16" />
+    <div class="cell 17" />
+    <div class="cell 18" />
+    <div class="cell 19" />
+    <div class="cell 20" />
+    <div class="cell 21" />
+    <div class="cell 22" />
+    <div class="cell 23" />
+    <div class="cell 24" />
+    <div class="cell 25" />
+    <div class="cell 26" />
+    <div class="cell 27" />
+    <div class="cell 28" />
+    <div class="cell 29" />
+    <div class="cell 30" />
+    <div class="cell 31" />
+    <div class="cell 32" />
+    <div class="cell 33" />
+    <div class="cell 34" />
+    <div class="cell 35" />
+    <div class="cell 36" />
+    <div class="cell 37" />
+    <div class="cell 38" />
+    <div class="cell 39" />
+    <div class="cell 40" />
+    <div class="cell 41" />
+    <div class="cell 42" />
+    <div class="cell 43" />
+    <div class="cell 44" />
+    <div class="cell 45" />
+    <div class="cell 46" />
+    <div class="cell 47" />
+    <div class="cell 48" />
+    <div class="cell 49" />
+    <div class="cell 50" />
+    <div class="cell 51" />
+    <div class="cell 52" />
+    <div class="cell 53" />
+    <div class="cell 54" />
+    <div class="cell 55" />
+    <div class="cell 56" />
+    <div class="cell 57" />
+    <div class="cell 58" />
+    <div class="cell 59" />
+    <div class="cell 60" />
+    <div class="cell 61" />
+    <div class="cell 62" />
+    <div class="cell 63" />
+    <div class="cell 64" />
+    <div class="cell 65" />
+    <div class="cell 66" />
+    <div class="cell 67" />
+    <div class="cell 68" />
+    <div class="cell 69" />
+    <div class="cell 70" />
+    <div class="cell 71" />
+    <div class="cell 72" />
+    <div class="cell 73" />
+    <div class="cell 74" />
+    <div class="cell 75" />
+    <div class="cell 76" />
+    <div class="cell 77" />
+    <div class="cell 78" />
+    <div class="cell 79" />
+    <div class="cell 80" />
+    <div class="cell 81" />
+    <div class="cell 82" />
+    <div class="cell 83" />
+    <div class="cell 84" />
+    <div class="cell 85" />
+    <div class="cell 86" />
+    <div class="cell 87" />
+    <div class="cell 88" />
+    <div class="cell 89" />
+    <div class="cell 90" />
+    <div class="cell 91" />
+    <div class="cell 92" />
+    <div class="cell 93" />
+    <div class="cell 94" />
+    <div class="cell 95" />
+    <div class="cell 96" />
+    <div class="cell 97" />
+    <div class="cell 98" />
+    <div class="cell 99" />
+    <div class="cell 100" />
+  </div>
+</div>
+
+<style>
+  .wrapper {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .board {
+    display: grid;
+    grid-template-columns: repeat(10, 50px);
+    grid-template-rows: repeat(10, 50px);
+  }
+  .cell {
+    /* aspect-ratio: 1; */
+    margin: 1px;
+    border: 1px solid pink;
+  }
+</style>
